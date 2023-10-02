69b38e32642ed7a29dddef75b7be7b8ea036018e
Ankit Choudhary <sign.ankit14@gmail.com>
2023-10-03 01:53:34 +0530

    Added animations on AIPartner page

--- aidao/src/components/AiPartner.jsx
+++ aidao/src/components/AiPartner.jsx
@@ -17,6 +17,50 @@ const container = {
 		},
 	},
 };
+const container1 = {
+	hidden: { opacity: 1, scale: 0 },
+	visible: {
+		opacity: 1,
+		scale: 1,
+		transition: {
+			delayChildren: 0.6,
+			staggerChildren: 1.5,
+		},
+	},
+};
+const container2 = {
+	hidden: { opacity: 1, scale: 0 },
+	visible: {
+		opacity: 1,
+		scale: 1,
+		transition: {
+			delayChildren: 1.0,
+			staggerChildren: 1.5,
+		},
+	},
+};
+const container3 = {
+	hidden: { opacity: 1, scale: 0 },
+	visible: {
+		opacity: 1,
+		scale: 1,
+		transition: {
+			delayChildren: 1.4,
+			staggerChildren: 1.5,
+		},
+	},
+};
+const container4 = {
+	hidden: { opacity: 1, scale: 0 },
+	visible: {
+		opacity: 1,
+		scale: 1,
+		transition: {
+			delayChildren: 1.8,
+			staggerChildren: 1.5,
+		},
+	},
+};
 
 const item = {
 	hidden: { y: 20, opacity: 0 },
@@ -41,23 +85,99 @@ export default function AiPartner() {
 	return (
 		<div className="flex flex-col items-center justify-center pt-5 mb-24 gap-20 lg:flex-row">
 			<div className="flex flex-col items-center justify-center lg:w-1/2 lg:items-start lg:ml-14">
-				<img
-					src={robot}
-					className="w-[20.356vw] h-[32.570vw] lg:h-80 lg:w-52 lg:ml-20"
-				/>
+				<motion.ul
+					className="flex flex-col items-center justify-center lg:ml-14"
+					ref={ref}
+					variants={container1}
+					initial="hidden"
+					animate={controls}
+					transition={{ staggerChildren: 0.2 }}
+				>
+					{/* Each item in the sequence */}
+					{[0].map((index) => (
+						<motion.li key={index} className="item" variants={item}>
+							{/* Text and tick image for each step */}
+
+							<div className="flex flex-col justify-center w-[20.356vw] h-[32.570vw] lg:h-80 lg:w-52 items-center">
+								{/* Text for each step */}
+								{index === 0 && (
+									<img
+										src={robot}
+										className="w-[20.356vw] h-[32.570vw] lg:h-80 lg:w-52 lg:ml-20 "
+									/>
+								)}
+							</div>
+						</motion.li>
+					))}
+				</motion.ul>
 
 				<div className="flex flex-col items-center justify-center gap-5 lg:items-start">
-					<div className="Inter text-[3.562vw] text-center font-bold bg-trainAIGradient text-transparent bg-clip-text lg:text-2xl">
-						Streamline Decision Making
-					</div>
-					<div className="Inter text-white text-[6.107vw] text-center font-extrabold lg:text-[42px] lg:w-[413px] lg:text-start">
-						Your AI Governance Partner
-					</div>
-					<div className="Inter text-white text-[4.071vw] text-center font-normal px-2 lg:text-2xl lg:w-11/12 lg:text-start">
-						Daig's smart contracts will define the token properties and govern
-						the platform's core functionalities, such as voting, staking, and
-						rewards distribution
-					</div>
+					<motion.ul
+						className="container flex flex-col items-center lg:items-start"
+						ref={ref}
+						variants={container2}
+						initial="hidden"
+						animate={controls}
+						transition={{ staggerChildren: 0.2 }}
+					>
+						{/* Each item in the sequence */}
+						{[0].map((index) => (
+							<motion.li key={index} className="item" variants={item}>
+								{/* Text and tick image for each step */}
+								<div className="flex">
+									<div className="Inter text-[3.562vw] text-center font-bold bg-trainAIGradient text-transparent bg-clip-text lg:text-2xl">
+										{/* Text for each step */}
+										{index === 0 && "Streamline Decision Making"}
+									</div>
+								</div>
+							</motion.li>
+						))}
+					</motion.ul>
+
+					<motion.ul
+						className="container flex flex-col items-center lg:items-start"
+						ref={ref}
+						variants={container3}
+						initial="hidden"
+						animate={controls}
+						transition={{ staggerChildren: 0.2 }}
+					>
+						{/* Each item in the sequence */}
+						{[0].map((index) => (
+							<motion.li key={index} className="item" variants={item}>
+								{/* Text and tick image for each step */}
+								<div className="flex">
+									<div className="Inter text-white text-[6.107vw] text-center font-extrabold lg:text-[42px] lg:w-[413px] lg:text-start">
+										{/* Text for each step */}
+										{index === 0 && "Your AI Governance Partner"}
+									</div>
+								</div>
+							</motion.li>
+						))}
+					</motion.ul>
+
+					<motion.ul
+						className="container flex flex-col items-center lg:items-start"
+						ref={ref}
+						variants={container4}
+						initial="hidden"
+						animate={controls}
+						transition={{ staggerChildren: 0.2 }}
+					>
+						{/* Each item in the sequence */}
+						{[0].map((index) => (
+							<motion.li key={index} className="item" variants={item}>
+								{/* Text and tick image for each step */}
+								<div className="flex">
+									<div className="Inter text-white text-[4.071vw] text-center font-normal px-2 lg:text-2xl lg:w-11/12 lg:text-start">
+										{/* Text for each step */}
+										{index === 0 &&
+											"Daig's smart contracts will define the token properties and govern the platform's core functionalities, such as voting, staking, and rewards distribution"}
+									</div>
+								</div>
+							</motion.li>
+						))}
+					</motion.ul>
 				</div>
 			</div>
 			<div
